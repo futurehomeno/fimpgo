3576684ceb0d98049206fe59bc491a3deaf9ce18
Aleksandrs Livincovs <aleksandrs.livincovs@gmail.com>
2017-05-26 01:07:23 +0200

    Init

---
+++ message.go
@@ -0,0 +1,318 @@
+package fimpgo
+
+import "time"
+import (
+	"github.com/buger/jsonparser"
+	"encoding/json"
+	"errors"
+)
+
+
+const (
+	VTypeString     = "string"
+	VTypeInt        = "int"
+	VTypeFloat      = "float"
+	VTypeBool       = "bool"
+	VTypeStrMap     = "str_map"
+	VTypeIntMap     = "int_map"
+	VTypeFloatMap   = "float_map"
+	VTypeBoolMap    = "bool_map"
+	VTypeStrArray   = "str_array"
+	VTypeIntArray   = "int_array"
+	VTypeFloatArray = "float_array"
+	VTypeBoolArray  = "bool_array"
+	VTypeObject     = "object"
+	VTypeNull       = "null"
+)
+
+type Props map[string]string
+type Tags []string
+
+type FimpMessage struct {
+	Type          string		`json:"type"`
+	Service       string 		`json:"serv"`
+	ValueType     string  	   	`json:"val_t"`
+	Value         interface{}	`json:"val"`
+	ValueObj      []byte		`json:"-"`
+	Tags          Tags		`json:"tags"`
+	Properties    Props		`json:"props"`
+	Version       string		`json:"ver"`
+	CorrelationID string		`json:"corid"`
+	CreationTime  time.Time		`json:"ctime"`
+	UID           string		`json:"uid"`
+}
+
+func (msg *FimpMessage) SetValue(value interface{},valType string){
+	msg.Value = value
+	msg.ValueType = valType
+}
+
+func (msg *FimpMessage) GetIntValue()(int64,error){
+	val,ok := msg.Value.(int64)
+	if ok {
+		return  val,nil
+	}
+	return 0,errors.New("Wrong value type")
+}
+
+func (msg *FimpMessage) GetStringValue()(string,error){
+	val,ok := msg.Value.(string)
+	if ok {
+		return  val,nil
+	}
+	return "",errors.New("Wrong value type")
+}
+
+func (msg *FimpMessage) GetBoolValue()(bool,error){
+	val,ok := msg.Value.(bool)
+	if ok {
+		return  val,nil
+	}
+	return false,errors.New("Wrong value type")
+}
+
+func (msg *FimpMessage) GetFloatValue()(float64,error){
+	val,ok := msg.Value.(float64)
+	if ok {
+		return  val,nil
+	}
+	return 0,errors.New("Wrong value type")
+}
+
+func (msg *FimpMessage) GetStrArrayValue()([]string,error){
+	val,ok := msg.Value.([]string)
+	if ok {
+		return  val,nil
+	}
+	return nil,errors.New("Wrong value type")
+}
+
+func (msg *FimpMessage) GetIntArrayValue()([]int64,error){
+	val,ok := msg.Value.([]int64)
+	if ok {
+		return  val,nil
+	}
+	return nil,errors.New("Wrong value type")
+}
+
+func (msg *FimpMessage) GetFloatArrayValue()([]float64,error){
+	val,ok := msg.Value.([]float64)
+	if ok {
+		return  val,nil
+	}
+	return nil,errors.New("Wrong value type")
+}
+
+func (msg *FimpMessage) GetBoolArrayValue()([]bool,error){
+	val,ok := msg.Value.([]bool)
+	if ok {
+		return  val,nil
+	}
+	return nil,errors.New("Wrong value type")
+}
+
+
+func (msg *FimpMessage) GetStrMapValue()(map[string]string,error){
+	val,ok := msg.Value.(map[string]string)
+	if ok {
+		return  val,nil
+	}
+	return nil,errors.New("Wrong value type")
+}
+
+func (msg *FimpMessage) GetIntMapValue()(map[string]int64,error){
+	val,ok := msg.Value.(map[string]int64)
+	if ok {
+		return  val,nil
+	}
+	return nil,errors.New("Wrong value type")
+}
+
+func (msg *FimpMessage) GetFloatMapValue()(map[string]float64,error){
+	val,ok := msg.Value.(map[string]float64)
+	if ok {
+		return  val,nil
+	}
+	return nil,errors.New("Wrong value type")
+}
+
+func (msg *FimpMessage) GetBoolMapValue()(map[string]bool,error){
+	val,ok := msg.Value.(map[string]bool)
+	if ok {
+		return  val,nil
+	}
+	return nil,errors.New("Wrong value type")
+}
+
+
+func (msg *FimpMessage)GetRawObjectValue()([]byte){
+	return msg.ValueObj
+}
+
+func (msg *FimpMessage)GetObjectValue(objectBindVar interface{})error{
+	return json.Unmarshal(msg.ValueObj,objectBindVar)
+}
+
+func (msg *FimpMessage) Serialize() ([]byte,error){
+	jsonBA,err := json.Marshal(msg)
+	return jsonBA,err
+	
+}
+
+func NewMessage(type_ string, service string, valueType string, value interface{}, props Props , tags Tags, requestMessage *FimpMessage) *FimpMessage {
+	msg := FimpMessage{Type: type_,
+		Service:         service,
+		ValueType:       valueType,
+		Value:           value,
+		Tags:            tags,
+		Properties:      props,
+		Version:         "1",
+	}
+	if requestMessage != nil {
+		msg.CorrelationID = requestMessage.UID
+	}
+	return &msg
+}
+
+func NewNullMessage(type_ string, service string, props Props , tags Tags, requestMessage *FimpMessage) *FimpMessage {
+	return NewMessage(type_ , service , VTypeNull, nil , props , tags , requestMessage )
+}
+
+func NewStringMessage(type_ string, service string, value string, props Props , tags Tags, requestMessage *FimpMessage) *FimpMessage {
+	return NewMessage(type_ , service , VTypeString, value , props , tags , requestMessage )
+}
+
+func NewIntMessage(type_ string, service string, value int64, props Props , tags Tags, requestMessage *FimpMessage) *FimpMessage {
+	return NewMessage(type_ , service , VTypeInt, value , props , tags , requestMessage )
+}
+
+func NewFloatMessage(type_ string, service string, value float64, props Props , tags Tags, requestMessage *FimpMessage) *FimpMessage {
+	return NewMessage(type_ , service , VTypeFloat, value , props , tags , requestMessage )
+}
+
+func NewBoolMessage(type_ string, service string, value bool, props Props , tags Tags, requestMessage *FimpMessage) *FimpMessage {
+	return NewMessage(type_ , service , VTypeBool, value , props , tags , requestMessage )
+}
+
+func NewStrArrayMessage(type_ string, service string, value []string, props Props , tags Tags, requestMessage *FimpMessage) *FimpMessage {
+	return NewMessage(type_ , service , VTypeStrArray, value , props , tags , requestMessage )
+}
+
+func NewIntArrayMessage(type_ string, service string, value []int64, props Props , tags Tags, requestMessage *FimpMessage) *FimpMessage {
+	return NewMessage(type_ , service , VTypeIntArray, value , props , tags , requestMessage )
+}
+
+func NewFloatArrayMessage(type_ string, service string, value []float64, props Props , tags Tags, requestMessage *FimpMessage) *FimpMessage {
+	return NewMessage(type_ , service , VTypeFloatArray, value , props , tags , requestMessage )
+}
+
+func NewBoolArrayMessage(type_ string, service string, value []bool, props Props , tags Tags, requestMessage *FimpMessage) *FimpMessage {
+	return NewMessage(type_ , service , VTypeBoolArray, value , props , tags , requestMessage )
+}
+
+func NewStrMapMessage(type_ string, service string, value map[string]string, props Props , tags Tags, requestMessage *FimpMessage) *FimpMessage {
+	return NewMessage(type_ , service , VTypeStrMap, value , props , tags , requestMessage )
+}
+
+func NewIntMapMessage(type_ string, service string, value map[string]int64, props Props , tags Tags, requestMessage *FimpMessage) *FimpMessage {
+	return NewMessage(type_ , service , VTypeIntMap, value , props , tags , requestMessage )
+}
+
+func NewFloatMapMessage(type_ string, service string, value map[string]float64, props Props , tags Tags, requestMessage *FimpMessage) *FimpMessage {
+	return NewMessage(type_ , service , VTypeFloatMap, value , props , tags , requestMessage )
+}
+
+func NewBoolMapMessage(type_ string, service string, value map[string]bool, props Props , tags Tags, requestMessage *FimpMessage) *FimpMessage {
+	return NewMessage(type_ , service , VTypeFloatMap, value , props , tags , requestMessage )
+}
+
+func NewMessageFromBytes(msg []byte)(*FimpMessage,error){
+	fimpmsg := FimpMessage{}
+	var err error
+	fimpmsg.Type ,err = jsonparser.GetString(msg,"type");
+	fimpmsg.Service ,err = jsonparser.GetString(msg,"serv");
+	fimpmsg.ValueType ,err = jsonparser.GetString(msg,"val_t");
+	switch fimpmsg.ValueType {
+	case VTypeString:
+		fimpmsg.Value,err = jsonparser.GetString(msg,"val");
+	case VTypeBool:
+		fimpmsg.Value,err = jsonparser.GetBoolean(msg,"val");
+	case VTypeInt :
+		fimpmsg.Value,err = jsonparser.GetInt(msg,"val");
+	case VTypeFloat:
+		fimpmsg.Value,err = jsonparser.GetInt(msg,"val");
+	case VTypeBoolArray:
+		val := []bool{}
+		jsonparser.ArrayEach(msg, func(value []byte, dataType jsonparser.ValueType, offset int, err error) {
+			item , _ := jsonparser.GetBoolean(value)
+			val = append(val,item)
+		}, "val")
+
+		fimpmsg.Value = val
+	case VTypeStrArray:
+		val := []string{}
+		jsonparser.ArrayEach(msg, func(value []byte, dataType jsonparser.ValueType, offset int, err error) {
+			item , _ := jsonparser.GetString(value)
+			val = append(val,item)
+		}, "val")
+
+		fimpmsg.Value = val
+	case VTypeIntArray:
+		val := []int64{}
+		jsonparser.ArrayEach(msg, func(value []byte, dataType jsonparser.ValueType, offset int, err error) {
+			item , _ := jsonparser.GetInt(value)
+			val = append(val,item)
+		}, "val")
+		fimpmsg.Value = val
+	case VTypeFloatArray:
+		val := []float64{}
+		jsonparser.ArrayEach(msg, func(value []byte, dataType jsonparser.ValueType, offset int, err error) {
+			item , _ := jsonparser.GetFloat(value)
+			val = append(val,item)
+		}, "val")
+		fimpmsg.Value = val
+
+	case VTypeStrMap:
+		val := make(map[string]string)
+		jsonparser.ObjectEach(msg, func(key []byte, value []byte, dataType jsonparser.ValueType, offset int) error {
+			val[string(key)],err = jsonparser.GetString(value)
+			return nil
+		}, "val")
+		fimpmsg.Value = val
+
+	case VTypeIntMap:
+		val := make(map[string]int64)
+		jsonparser.ObjectEach(msg, func(key []byte, value []byte, dataType jsonparser.ValueType, offset int) error {
+			val[string(key)],err = jsonparser.GetInt(value)
+			return nil
+		}, "val")
+		fimpmsg.Value = val
+
+	case VTypeFloatMap:
+		val := make(map[string]float64)
+		jsonparser.ObjectEach(msg, func(key []byte, value []byte, dataType jsonparser.ValueType, offset int) error {
+			val[string(key)],err = jsonparser.GetFloat(value)
+			return nil
+		}, "val")
+		fimpmsg.Value = val
+
+	case VTypeBoolMap:
+		val := make(map[string]bool)
+		jsonparser.ObjectEach(msg, func(key []byte, value []byte, dataType jsonparser.ValueType, offset int) error {
+			val[string(key)],err = jsonparser.GetBoolean(value)
+			return nil
+		}, "val")
+		fimpmsg.Value = val
+
+	case VTypeObject:
+		fimpmsg.ValueObj,_,_,err = jsonparser.Get(msg,"val");
+
+	}
+
+
+	return &fimpmsg ,err
+
+}
+
+
+
---
+++ message_test.go
@@ -0,0 +1,163 @@
+package fimpgo
+
+import "testing"
+
+func TestNewBoolMessage(t *testing.T) {
+	msg :=  NewBoolMessage("cmd.binary.set", "out_bin_switch", true, nil , nil, nil)
+	val , err := msg.GetBoolValue();
+	if err != nil {
+		t.Error(err)
+	}
+	if  val == false {
+		t.Error("Wrong value")
+	}
+	t.Log("ok")
+}
+
+func TestNewFloatMessage(t *testing.T) {
+
+	msg :=  NewFloatMessage("evt.sensor.report", "temp_sensor", float64(35.5), nil , nil, nil)
+	val , err := msg.GetFloatValue();
+	if err != nil {
+		t.Error(err)
+	}
+	if  val != 35.5 {
+		t.Error("Wrong value")
+	}
+	t.Log("ok")
+}
+
+
+func TestFimpMessage_SerializeBool(t *testing.T) {
+	msg :=  NewBoolMessage("cmd.binary.set", "out_bin_switch", true, nil , nil, nil)
+	serVal , err := msg.Serialize()
+	if err != nil {
+		t.Error(err)
+	}
+	t.Log(string(serVal))
+}
+
+func TestFimpMessage_SerializeFloat(t *testing.T) {
+	props := Props{}
+	props["unit"] = "C"
+	msg :=  NewFloatMessage("evt.sensor.report", "temp_sensor", float64(35.5), props , nil, nil)
+	serVal , err := msg.Serialize()
+	if err != nil {
+		t.Error(err)
+	}
+	t.Log(string(serVal))
+
+}
+
+func BenchmarkFimpMessage_Serialize(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		msg :=  NewBoolMessage("cmd.binary.set", "out_bin_switch", true, nil , nil, nil)
+		_ , err := msg.Serialize()
+		if err != nil {
+			b.Error(err)
+		}
+	}
+}
+
+func BenchmarkFimpMessage_Serialize2(b *testing.B) {
+	props := make(map[string]string)
+	props["param1"] = "val1"
+	for i := 0; i < b.N; i++ {
+		msg :=  NewStrMapMessage("cmd.config.set", "dev_sys", props, nil , nil, nil)
+		_ , err := msg.Serialize()
+		if err != nil {
+			b.Error(err)
+		}
+	}
+}
+
+func TestNewMessageFromBytes(t *testing.T) {
+	msgString := "{\"serv\":\"out_bin_switch\",\"type\":\"cmd.binary.set\",\"val_t\":\"bool\",\"val\":true,\"props\":null,\"tags\":null}"
+	fimp,err := NewMessageFromBytes([]byte(msgString))
+	if err != nil {
+		t.Error(err)
+	}
+	val , err := fimp.GetBoolValue();
+	if  val == false {
+		t.Error("Wrong value")
+	}
+	t.Log("ok")
+}
+
+func TestFimpMessage_GetStrMapValue(t *testing.T) {
+	msgString := "{\"serv\":\"dev_sys\",\"type\":\"cmd.config.set\",\"val_t\":\"str_map\",\"val\":{\"param1\":\"val1\",\"param2\":\"val2\"},\"props\":null,\"tags\":null}"
+	fimp,err := NewMessageFromBytes([]byte(msgString))
+	if err != nil {
+		t.Error(err)
+	}
+
+	val ,err := fimp.GetStrMapValue()
+	if err != nil {
+		t.Error(err)
+	}
+	if val["param2"] != "val2" {
+		t.Error("Wrong map result")
+	}
+}
+
+func BenchmarkFimpMessage_GetStrMapValue(b *testing.B) {
+	msgString := []byte("{\"serv\":\"dev_sys\",\"type\":\"cmd.config.set\",\"val_t\":\"str_map\",\"val\":{\"param1\":\"val1\",\"param2\":\"val2\"},\"props\":null,\"tags\":null}")
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		fimp,err := NewMessageFromBytes(msgString)
+		if err != nil {
+			b.Error(err)
+		}
+
+		val ,err := fimp.GetStrMapValue()
+		if err != nil {
+			b.Error(err)
+		}
+		if val["param2"] != "val2" {
+			b.Error("Wrong map result")
+		}
+	}
+}
+
+func TestFimpMessage_GetObjectValue(t *testing.T) {
+	type Config struct {
+		Param1 string
+		Param2 string
+	}
+	msgString := "{\"serv\":\"dev_sys\",\"type\":\"cmd.config.set\",\"val_t\":\"object\",\"val\":{\"param1\":\"val1\",\"param2\":\"val2\"},\"props\":null,\"tags\":null}"
+	fimp,err := NewMessageFromBytes([]byte(msgString))
+	if err != nil {
+		t.Error(err)
+	}
+	config := Config{}
+	err = fimp.GetObjectValue(&config)
+	if err != nil {
+		t.Error(err)
+	}
+	if config.Param2 != "val2" {
+		t.Error("Wrong map result")
+	}
+}
+
+func BenchmarkFimpMessage_GetObjectValue(b *testing.B) {
+	type Config struct {
+		Param1 string
+		Param2 string
+	}
+	msgString := []byte("{\"serv\":\"dev_sys\",\"type\":\"cmd.config.set\",\"val_t\":\"object\",\"val\":{\"param1\":\"val1\",\"param2\":\"val2\"},\"props\":null,\"tags\":null}")
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		fimp, err := NewMessageFromBytes(msgString)
+		if err != nil {
+			b.Error(err)
+		}
+		config := Config{}
+		err = fimp.GetObjectValue(&config)
+		if err != nil {
+			b.Error(err)
+		}
+		if config.Param2 != "val2" {
+			b.Error("Wrong map result")
+		}
+	}
+}
