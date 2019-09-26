c19462baf8df6ef882be54b7862f4d3d1ab34aac
Arun Poudel <arun@futurehome.no>
2019-09-26 18:14:27 +0200

    vincservice fix

--- fimptype/primefimp/api-client.go
+++ fimptype/primefimp/api-client.go
@@ -197,21 +197,20 @@ func (mh *ApiClient) GetShortcuts(fromCache bool) ([]Shortcut, error) {
 	return nil, nil
 }
 
-func (mh *ApiClient) GetVincServices(fromCache bool) (map[string]interface{}, error) {
+func (mh *ApiClient) GetVincServices(fromCache bool) (VincServices, error) {
 	if !fromCache {
 		fimpResponse, err := mh.sendGetRequest([]string{ComponentService})
 		if err != nil {
-			return nil, err
+			return VincServices{}, err
 		}
 		response, err := FimpToResponse(fimpResponse)
 		if err != nil {
-			return nil, err
+			return VincServices{}, err
 		}
 		return response.GetVincServices(), err
 	}
-	return nil, nil
+	return VincServices{}, nil
 }
-
 
 func (mh *ApiClient) GetSite(fromCache bool) (*Site, error) {
 	if !fromCache {
