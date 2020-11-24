faffe425e9e6e058a5a85272d1e3271000b3e2dc
Vlad Datcu <vlad.datcu@gmail.com>
2020-11-24 14:53:04 +0200

    - add state filtering on pointer types

--- fimptype/primefimp/option.go
+++ fimptype/primefimp/option.go
@@ -1,8 +1,24 @@
 package primefimp
 
-type apiClientConfig struct {
-	cloudService string
-}
+import (
+	"github.com/futurehomeno/fimpgo"
+	"time"
+)
+
+type (
+	connectionPoolConfig struct {
+		clientIdPrefix          string
+		initialSize             int
+		minSize                 int
+		maxSize                 int
+		connectionConfiguration fimpgo.MqttConnectionConfigs
+		lifetime                time.Duration
+	}
+	apiClientConfig struct {
+		cloudService   string
+		connectionPool *connectionPoolConfig
+	}
+)
 
 type Option interface {
 	apply(*apiClientConfig)
@@ -16,4 +32,44 @@ func (cso cloudServiceOption) apply(config *apiClientConfig) {
 
 func WithCloudService(service string) Option {
 	return cloudServiceOption(service)
+}
+
+type connectionPoolOption struct {
+	clientIdPrefix          string
+	initialSize             int
+	minSize                 int
+	maxSize                 int
+	connectionConfiguration fimpgo.MqttConnectionConfigs
+	lifetime                time.Duration
+}
+
+func (cpo connectionPoolOption) apply(config *apiClientConfig) {
+	config.connectionPool = &connectionPoolConfig{
+		clientIdPrefix:          cpo.clientIdPrefix,
+		initialSize:             cpo.initialSize,
+		maxSize:                 cpo.maxSize,
+		minSize:                 cpo.minSize,
+		connectionConfiguration: cpo.connectionConfiguration,
+		lifetime:                cpo.lifetime,
+	}
+
+}
+
+func WithConnectionPool(clientIdPrefix string, initialSize, minSize, maxSize int, lifetime time.Duration, connectionConfiguration fimpgo.MqttConnectionConfigs) Option {
+	if lifetime == 0 {
+		lifetime = 20 * time.Second
+	}
+	if initialSize < 0 {
+		initialSize = 0
+	}
+
+	if minSize < 1 {
+		minSize = 1
+	}
+
+	if maxSize > 100 {
+		maxSize = 100
+	}
+
+	return connectionPoolOption{clientIdPrefix, initialSize, minSize, maxSize, connectionConfiguration, lifetime}
 }
