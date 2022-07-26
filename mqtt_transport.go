ac615a0a450d4d8eb7119d2914affe67a6969a3e
BudzikM <michal@futurehome.no>
2022-07-26 19:31:57 +0200

    Add Option()

--- mqtt_transport.go
+++ mqtt_transport.go
@@ -85,6 +85,10 @@ func (mh *MqttTransport) SetCertDir(certDir string) {
 	mh.certDir = certDir
 }
 
+func (mh *MqttTransport) Options() *MQTT.ClientOptions {
+	return mh.mqttOptions
+}
+
 type MessageHandler func(topic string, addr *Address, iotMsg *FimpMessage, rawPayload []byte)
 
 // NewMqttTransport constructor. serverUri="tcp://localhost:1883"
